bac3aad66a32411d5e41d43e84952998c9126e71
Robert Randolph <rrandol3@uwyo.edu>
2018-10-17 14:27:02 -0600

    Implimented basic user interface using command system.

--- src/Interface.java
+++ src/Interface.java
@@ -14,23 +14,78 @@
  * Play music again || New music generation || new midi file || done
  */
 
+import java.util.Scanner;
+
 public class Interface {
+    
+    // Constants
+    private static final String COMMANDS =
+            "Commands:\n"
+            + "LOAD :: Loads a given midi file. Automatically generates music with length 10.\n"
+            + "GEN  :: Generates new music given a length using exisitng midi data.\n"
+            + "SAVE :: Saves the generated music as a midi file.\n"
+            + "PLAY :: Plays the generated music.\n"
+            + "DONE :: Exits program.\n"
+            + "HELP :: Gives a list of commands.\n";
     
     // Fields
     private MusicGenerator mgen;
     private String filename;
+    private String input;
+    private int length;
+    private boolean done;
 
     // Constructor
     public Interface()
     {
         // Init
+        Scanner scanner = new Scanner(System.in);
         mgen = new MusicGenerator();
         
-        // get filename from user
-        // push filename to music generator
-        mgen.loadMidiFile(filename);
-        mgen.generateMusic(10);
-        mgen.playSong();
-        mgen.saveSong();
+        // Intro
+        System.out.println(COMMANDS);
+        
+        // User
+        done = false;
+        while (!done) {
+            // Prompt
+            System.out.print("Command: ");
+            input = scanner.nextLine();
+            
+            // LOAD
+            if (input.equalsIgnoreCase("LOAD")) {
+                System.out.print("Midi filename: ");
+                filename = scanner.nextLine();
+                mgen.loadMidiFile(filename);
+                mgen.generateMusic(10);
+            }
+            // GEN
+            else if (input.equalsIgnoreCase("GEN")) {
+                System.out.print("Music length: ");
+                length = scanner.nextInt();
+                if (scanner.hasNextLine())
+                    scanner.nextLine(); // Gets rid of excess input.
+                mgen.generateMusic(length);
+            }
+            // SAVE
+            else if (input.equalsIgnoreCase("SAVE"))
+                mgen.saveSong();
+            // PLAY
+            else if (input.equalsIgnoreCase("PLAY"))
+                mgen.playSong();
+            // DONE
+            else if (input.equalsIgnoreCase("DONE"))
+                done = true;
+            // HELP
+            else if (input.equalsIgnoreCase("HELP"))
+                System.out.println(COMMANDS);
+            // Unknown
+            else
+                System.out.println("Unknown Command. Type HELP for a list.");
+            }
+        
+        // Closing
+        scanner.close();
+        System.out.println("Exited Program");
     }
 }
