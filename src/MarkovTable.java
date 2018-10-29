1b99b6485efafffaabcc51d0ce9629c8f791d651
T-Factorial <[taylorbleizeffer@gmail.com]>
2018-10-28 23:49:30 -0600

    Handed data read from midi to MarkovTable. Filled in MarkovTable's generateTable function, which now stores probability of interval occurance in a HashMap. Created custom key, MarkovKey, class to use as HashMap key. It holds two pitch values. TODO: Add get/put to MarkovTable class? Which will then be used by MusicGenerator class?

--- src/MarkovTable.java
+++ src/MarkovTable.java
@@ -1,3 +1,7 @@
+import javafx.util.Pair;
+import jm.music.data.Note;
+import java.util.*;
+
 /**
  * @filename MarkovTable
  * @project Procedural Music
@@ -5,12 +9,27 @@
  * 
  * Creates a table from the midi data read from the MidiReader.
  * Signals the Music generation when done.
+ * Table is actually HashMap
+ * The Key is a custom key that holds two values
+ * which are the Pitchs of an interval.
+ * Holds the percentage of all intervals
+ * that the interval occurs
+ *
+ * To add values:
+ * MarkovKey key = new MarkovKey(int pitchConst1, int pitchConst2)
+ * (where the pitchConsts are pitch constants found in JM)
+ * markovTable.put(key, value)
+ *
+ * To get values:
+ * double prob = markovTable.get(new MarkovKey(JMC.C4, JMC.D4);
  */
 
 public class MarkovTable {
-    
+
     // Fields
     private MidiReader midiReader;
+    private Note[] notes;
+    private Map<MarkovKey, Double> markovTable;
     //*** private Music music
     
     // Constructor
@@ -21,15 +40,38 @@ public class MarkovTable {
     // Loads midi file
     public void loadMidiFile(String filename) {
         // Get midi data for processing.
-        midiReader.readMidi(filename);
+        notes = midiReader.readMidi(filename);
     }
     
     // Generate a music table given midi data
     // Then creates music.
-    public int[][] generateTable(/*midi data*/) {
-        int[][] table = new int[5][5];
+    public /*Map<MarkovKey, Double>*/ void generateTable(Note[] notes) {
+        markovTable = new HashMap<MarkovKey, Double>();
 
-        return table;
+        Note note; // examining interval from note
+        Note next; // to next note
+        int intervals = 0;  // accumulator for occurances of intervals
+        double occurs = 0;  // probability of this interval
+        for (int i = 0; i < notes.length - 1; ++i) {
+            note = notes[i];    // Examining all intervals starting with this note
+            next = notes[i + 1];
+            ++intervals;
+            // search rest of notes for more occurances of interval
+            for (int j = i + 1; j < notes.length - 1; ++j) {
+                if (notes[j] != note)
+                    continue;   // sift until we find another occurance of the first note
+                if (notes[j + 1] != next)
+                    continue;   // if first note matches but the 2nd does not, keep sifting
+                ++intervals; // should only reach here if notes[j] == note && notes[j + 1] == next
+            }
+
+            // Generate Key for Interval
+            // Calculate probability of interval occurance
+            // Add Interval entry to our HashTable (Markov Table)
+            MarkovKey key = new MarkovKey(note.getPitch(), next.getPitch());
+            occurs = intervals / notes.length;
+            markovTable.put(key, occurs);
+        }
     }
 
 }
--- src/MidiReader.java
+++ src/MidiReader.java
@@ -8,23 +8,35 @@
  */
 
 import jm.music.data.*;
-imoprt jm.music.tools.*;
+import jm.music.tools.*;
+import jm.util.Read;
+
+import java.util.Vector;
 
 public class MidiReader {
     
     // Constructor
-    public MidiReader()
-    {
+    public MidiReader() {
         
     }
     
     // Given a filename it will attempt to read a midi file.
     // Returns the midi data for processing.
-    public void/*midi data*/ readMidi(String filename) {
+    // Currently returns a Vector of Notes (in the order they appear in input)
+    public Note[] readMidi(String filename) {
         // with midi filename read in midi data
+
+        // Read Score from midi file
         Score from_midi = new Score("midi_input");
         Read.midi(from_midi, filename);
 
+        // Might need reworked
+        // i.e., needs to check whether "Phrases" and "Parts" actually exists in the score.
+        Note[] notes = from_midi.getPart(0)
+                .getPhrase(0)
+                .getNoteArray();
+
+        return notes;
     }
 
 }
