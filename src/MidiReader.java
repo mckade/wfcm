ba59f04aff5e8b1a94663f59b15e70438e193404
Taylor Bleizeffer <taylorbleizeffer@gmail.com>
2019-02-13 22:25:01 -0700

    Started modifying MidiReader
    Started modifying MidiReader to extract data from raw SMF. Need to parse Event objects somehow.

--- src/MidiReader.java
+++ src/MidiReader.java
@@ -7,38 +7,71 @@
  * Returns the read data to be processed.
  */
 
-import jm.music.data.Note;
-import jm.music.data.Score;
+import jm.midi.MidiParser;
+import jm.midi.MidiUtil;
+import jm.midi.SMF;
+import jm.midi.Track;
+import jm.midi.event.Event;
+import jm.music.data.*;
 import jm.util.Read;
 
+import java.util.Vector;
+
 public class MidiReader {
-    
+
     // Constructor
     public MidiReader() {
-        
     }
     
     // Given a filename it will attempt to read a midi file.
     // Returns the midi data for processing.
     // Currently returns a Vector of Notes (in the order they appear in input)
     public Note[] readMidi(String filename) {
-        
+
+        Part[] parts = null;
+        Phrase[][] phrases = null;
         Note[] notes = null;
-        
+
         // with midi filename read in midi data
         // Read Score from midi file
         Score from_midi = new Score("midi_input");
         Read.midi(from_midi, filename);
-        
+
+        // Convert Score into raw SMF data (Standard MIDI File)
+        SMF smf = new SMF();
+        MidiParser.scoreToSMF(from_midi, smf);
+
+        // Get Tracks from SMF. Track 1 should be header data.
+        // Might need to check instanceof ?
+        Vector<Track> tracks = smf.getTrackList();
+        Vector<Vector<Event>> events = new Vector<>();
+
+        System.out.println("PPQN: " + smf.getPPQN());
+        System.out.println("Number of tracks: " + tracks.size());
+
+        // Get EventList from each Track
+        for (Track trk : tracks) {
+            events.add(trk.getEvtList());
+        }
+
+        // Get Event data
+        int i = 1;
+        for (Vector<Event> vec : events) {
+            System.out.println("\nTrack #" + i + " events:");
+            System.out.println("---------------------------------------");
+
+        }
+
         // Checking if file was successfully read.
         if (from_midi.size() != 0) {
             // Might need reworked
             // i.e., needs to check whether "Phrases" and "Parts" actually exists in the score.
+
             notes = from_midi.getPart(0)
                     .getPhrase(0)
                     .getNoteArray();
-        }
 
+        }
         return notes;
     }
 
