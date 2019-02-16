68e9417ba950288d605c97adc7bb9349b390d21c
Taylor Bleizeffer <taylorbleizeffer@gmail.com>
2019-02-15 21:52:42 -0700

    Event Parsing. Chord Detection/Building
    Figured out how to parse events from SMF. Started an algorithm to detect and build chords. Now need to figure out how to send them off along with regular notes in the correct sequence they're played.
    Going to change reading so read notes directly from SMF.
    Discovered that may have all been entirely too much work, as Phrases have a getNoteStartTime() method that I looked over. Although that may prove more complicated than just reading events. So we'll just hae to see.

--- src/MidiReader.java
+++ src/MidiReader.java
@@ -11,10 +11,14 @@ import jm.midi.MidiParser;
 import jm.midi.MidiUtil;
 import jm.midi.SMF;
 import jm.midi.Track;
-import jm.midi.event.Event;
+import jm.midi.event.*;
 import jm.music.data.*;
 import jm.util.Read;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Vector;
 
 public class MidiReader {
@@ -26,7 +30,7 @@ public class MidiReader {
     // Given a filename it will attempt to read a midi file.
     // Returns the midi data for processing.
     // Currently returns a Vector of Notes (in the order they appear in input)
-    public Note[] readMidi(String filename) {
+    public Note[] readMidi(String filename) throws IOException {
 
         Note[] notes = null;
 
@@ -35,7 +39,6 @@ public class MidiReader {
         Score from_midi = new Score("midi_input");
         Read.midi(from_midi, filename);
 
-        /*
 
         // Convert Score into raw SMF data (Standard MIDI File)
         SMF smf = new SMF();
@@ -46,8 +49,8 @@ public class MidiReader {
         Vector<Track> tracks = smf.getTrackList();
         Vector<Vector<Event>> events = new Vector<>();
 
-        System.out.println("PPQN: " + smf.getPPQN());
-        System.out.println("Number of tracks: " + tracks.size());
+        System.out.println("PPQN: " + smf.getPPQN()); // Pulses per Quarter Note
+        System.out.println("Number of tracks: " + tracks.size()); // Track 1 is header data
 
         // Get EventList from each Track
         for (Track trk : tracks) {
@@ -55,41 +58,165 @@ public class MidiReader {
         }
 
         // Get Event data
-        int i = 1;
-        for (Vector<Event> vec : events) {
+        int i = 1; // Keeping track of Track #
+        int j; // Event Vector index
+        for (Vector<Event> vec : events) { // Loop through Tracks
+
             System.out.println("\nTrack #" + i + " events:");
             System.out.println("---------------------------------------");
-            for (Event ev : vec) {
-                System.out.println(ev.toString());
+
+            ++i;
+            j = 0;
+
+            for (Event ev : vec) { // Loop through Events
+
+                short eventID = ev.getID(); // ID determines type of event
+
+                // Needed for chord tracking
+                CPhrase chord = new CPhrase();
+                Vector<Integer> pitches = new Vector<>();
+
+
+                // We're mostly interested in the NoteOn/NoteOff events for chords.
+                switch (eventID) {
+                    case 1: // ATouch event (Aftertouch. Polyphonic key pressure)
+                        ATouch touch = (ATouch) ev;
+                        System.out.println("ATouch event:");
+                        System.out.println("Pitch: " + touch.getPitch());
+                        System.out.println("Pressure: " + touch.getPressure());
+                        System.out.println("Time: " + touch.getTime());
+                        System.out.println("End Event: " + MidiUtil.getEndEvt(touch.getPitch(), vec, j));
+                        break;
+                    case 2: // CPres event (Channel pressure)
+                        CPres cpres = (CPres) ev;
+                        System.out.println("CPres event:");
+                        System.out.println("Pressure: " + cpres.getPressure());
+                        System.out.println("Time: " + cpres.getTime());
+                        break;
+                    case 3: // CChange event (Controller change)
+                        CChange cchange = (CChange) ev;
+                        System.out.println("CChange event:");
+                        System.out.println("Controller #: " + cchange.getControllerNum());
+                        System.out.println("Value: " + cchange.getValue());
+                        System.out.println("Time: " + cchange.getTime());
+                        break;
+                    case 4: // NoteOff event
+                        NoteOff off = (NoteOff) ev;
+                        System.out.println("NoteOff event:");
+                        System.out.println("Pitch: " + off.getPitch());
+                        System.out.println("Velocity: " + off.getVelocity());
+                        System.out.println("Time: " + off.getTime());
+                        System.out.println("End Event: " + MidiUtil.getEndEvt(off.getPitch(), vec, j));
+                        break;
+                    case 5: // NoteOn event
+                        // Some MIDI files encode NoteOns as NoteOffs with velocity=0
+
+                        NoteOn on = (NoteOn) ev;
+                        if (on.getVelocity() != 0) {
+                            System.out.println("NoteOn event:");
+
+                            // Add pitch value to vector
+                            pitches.add(Short.toUnsignedInt(on.getPitch()));
+
+                        } else {
+                            System.out.println("NoteOff event:");
+
+                            // Check if more than one pitch in vector
+                            if (pitches.size() > 1) {
+
+                                // CPhrase.addChord only takes int[] or Note[]
+                                // So, "converting" Vector<Integer> to int[]
+                                int[] ints = new int[pitches.size()];
+                                for (int k = 0; k < pitches.size(); ++k) {
+                                    ints[k] = pitches.get(k);
+                                }
+                                chord.addChord(ints, 1);
+                            }
+
+                            // Clear pitch vector for next chord.
+                            pitches.clear();
+                        }
+
+                        System.out.println("Pitch: " + on.getPitch());
+                        System.out.println("Velocity: " + on.getVelocity());
+                        System.out.println("Time: " + on.getTime());
+                        break;
+                    case 6: // PWheel event (Pitch wheel)
+                        PWheel wheel = (PWheel) ev;
+                        System.out.println("PWheel event:");
+                        System.out.println("Value: " + wheel.getValue());
+                        System.out.println("Time: " + wheel.getTime());
+                        break;
+                    case 7: // PChange event (Program change)
+                        PChange pchange = (PChange) ev;
+                        System.out.println("PChange event:");
+                        System.out.println("Value: " + pchange.getValue());
+                        System.out.println("Time: " + pchange.getTime());
+                        break;
+                    case 8: // SysEx event
+                        SysEx sysex = (SysEx) ev;
+                        System.out.println("SysEx event(s):");
+                        // Note sure what to do here. Has a method to return a list of System Exclusive events
+                        break;
+                    case 16: // Tempo event
+                        TempoEvent tempevt = (TempoEvent) ev;
+                        System.out.println("TempoEvent event:");
+                        System.out.println("Tempo: " + tempevt.getTempo());
+                        System.out.println("Time: " + tempevt.getTime());
+                        break;
+                    case 17: // TimeSig event
+                        TimeSig timesig = (TimeSig) ev;
+                        System.out.println("TimeSig event:");
+                        System.out.println("Time Signature: " + timesig.getNumerator()
+                                + "/" + timesig.getDenominator());
+                        System.out.println("Metronome Pulse: " + timesig.getMetronomePulse());
+                        System.out.println("Thirty-Second Notes per Beat: " + timesig.getThirtySecondNotesPerBeat());
+                        System.out.println("Time: " + timesig.getTime());
+                        break;
+                    case 18: // KeySig event
+                        KeySig key = (KeySig) ev;
+                        System.out.println("KeySig event:" );
+                        System.out.println("Key Signature: " + key.getKeySig());
+                        System.out.println("Key Quality: " + key.getKeyQuality());
+                        System.out.println("Time: " + key.getTime());
+                        break;
+                    case 23: // EndTrack event
+                        EndTrack end = (EndTrack) ev;
+                        System.out.println("EndTrack event:");
+                        System.out.println("Time:" + end.getTime());
+                        break;
+                    default:
+                        System.out.println("Unknown event type.");
+                        break;
+                }
+                ++j;
             }
         }
 
-        */
+
 
         // Checking if file was successfully read.
         if (from_midi.size() != 0) {
             // Might need reworked
             // i.e., needs to check whether "Phrases" and "Parts" actually exists in the score.
 
-            Vector<Phrase> phrases = from_midi.getPart(0).getPhraseList();
-            Vector<Vector<Note>> notess = new Vector<>();
-            System.out.println("Number of Phrases: " + phrases.size());
-            for (int i = 0; i < phrases.size(); ++i) {
-                System.out.println("----------------------------------");
-                System.out.print("Number of Notes in Phrase #" + i + ": ");
-                notess.add(phrases.get(i).getNoteList());
-                System.out.println(notess.get(i).size());
-                for (int j = 0; j < notess.get(i).size(); ++j) {
-                    if (!notess.get(i).get(j).isRest()) {
-                        System.out.println("Note #" + j + " pitch: " + notess.get(i).get(j).getPitch());
-                    }
-                }
-            }
-
+            /*
             notes = from_midi.getPart(0)
                     .getPhrase(0)
                     .getNoteArray();
+            */
 
+            /*
+            Vector<Phrase> phrases = from_midi.getPart(0).getPhraseList();
+            int count = 1;
+            for (Phrase p : phrases) {
+                Vector<Note> nts = p.getNoteList();
+                System.out.println("Phrase #" + count + ":");
+                for (int l = 0; l < nts.size(); ++l) {
+                    System.out.println("Note " + nts.get(l).getPitch() + " Start Time: " + p.getNoteStartTime(l));
+                }
+            }
+            */
         }
         return notes;
     }
