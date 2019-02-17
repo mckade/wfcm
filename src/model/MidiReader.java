c0261f40e74dc58b9c9c5ca6912c8a714609f1b8
Taylor Bleizeffer <taylorbleizeffer@gmail.com>
2019-02-17 14:27:19 -0700

    Cleaned up Event parsing
    Cleaned up Event parsing. Vector objects can't be resolved at the moment though?? Need to figure out how I'm going to hand over Chords and Notes to MarkovTable chronologically and how chords will be handled in the MarkovTable for music generation.

--- src/model/MidiReader.java
+++ src/model/MidiReader.java
@@ -16,13 +16,23 @@ import jm.midi.event.*;
 import jm.music.data.*;
 import jm.util.Read;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Vector;
 
 public class MidiReader {
+
+    // Inner class used to encapsulate data obtained form parsing
+    // Event object from SMF
+    public class MidiData {
+
+        CPhrase chords;
+        Note[] notes;
+
+        public MidiData(CPhrase c, Note[] n) {
+            this.chords = c;
+            this.notes = n;
+        }
+    }
 
     // Constructor
     public MidiReader() {
@@ -31,14 +41,14 @@ public class MidiReader {
     // Given a filename it will attempt to read a midi file.
     // Returns the midi data for processing.
     // Currently returns a Vector of Notes (in the order they appear in input)
-    public Note[] readMidi(String filename) throws IOException {
+    public Note[] readMidi(String/*File*/ filename) throws IOException {
 
         Note[] notes = null;
 
         // with midi filename read in midi data
         // Read Score from midi file
         Score from_midi = new Score("midi_input");
-        Read.midi(from_midi, filename);
+        Read.midi(from_midi, filename/*.getCanonicalPath()*/);
 
 
         // Convert Score into raw SMF data (Standard MIDI File)
@@ -66,137 +76,11 @@ public class MidiReader {
             System.out.println("\nTrack #" + i + " events:");
             System.out.println("---------------------------------------");
 
+            parseEvents(vec);
+
             ++i;
-            j = 0;
-
-            for (Event ev : vec) { // Loop through Events
-
-                short eventID = ev.getID(); // ID determines type of event
-
-                // Needed for chord tracking
-                CPhrase chord = new CPhrase();
-                Vector<Integer> pitches = new Vector<>();
-
-
-                // We're mostly interested in the NoteOn/NoteOff events for chords.
-                switch (eventID) {
-                    case 1: // ATouch event (Aftertouch. Polyphonic key pressure)
-                        ATouch touch = (ATouch) ev;
-                        System.out.println("ATouch event:");
-                        System.out.println("Pitch: " + touch.getPitch());
-                        System.out.println("Pressure: " + touch.getPressure());
-                        System.out.println("Time: " + touch.getTime());
-                        System.out.println("End Event: " + MidiUtil.getEndEvt(touch.getPitch(), vec, j));
-                        break;
-                    case 2: // CPres event (Channel pressure)
-                        CPres cpres = (CPres) ev;
-                        System.out.println("CPres event:");
-                        System.out.println("Pressure: " + cpres.getPressure());
-                        System.out.println("Time: " + cpres.getTime());
-                        break;
-                    case 3: // CChange event (Controller change)
-                        CChange cchange = (CChange) ev;
-                        System.out.println("CChange event:");
-                        System.out.println("Controller #: " + cchange.getControllerNum());
-                        System.out.println("Value: " + cchange.getValue());
-                        System.out.println("Time: " + cchange.getTime());
-                        break;
-                    case 4: // NoteOff event
-                        NoteOff off = (NoteOff) ev;
-                        System.out.println("NoteOff event:");
-                        System.out.println("Pitch: " + off.getPitch());
-                        System.out.println("Velocity: " + off.getVelocity());
-                        System.out.println("Time: " + off.getTime());
-                        System.out.println("End Event: " + MidiUtil.getEndEvt(off.getPitch(), vec, j));
-                        break;
-                    case 5: // NoteOn event
-                        // Some MIDI files encode NoteOns as NoteOffs with velocity=0
-
-                        NoteOn on = (NoteOn) ev;
-                        if (on.getVelocity() != 0) {
-
-                            System.out.println("NoteOn event:");
-
-                            // Add pitch value to vector
-                            pitches.add(Short.toUnsignedInt(on.getPitch()));
-
-                        } else {
-                            System.out.println("NoteOff event:");
-
-                            // Check if more than one pitch in vector
-                            if (pitches.size() > 1) {
-
-                                // CPhrase.addChord only takes int[] or Note[]
-                                // So, "converting" Vector<Integer> to int[]
-                                int[] ints = new int[pitches.size()];
-                                for (int k = 0; k < pitches.size(); ++k) {
-                                    ints[k] = pitches.get(k);
-                                }
-                                chord.addChord(ints, 1);
-                            }
-
-                            // Clear pitch vector for next chord.
-                            pitches.clear();
-                        }
-
-                        System.out.println("Pitch: " + on.getPitch());
-                        System.out.println("Velocity: " + on.getVelocity());
-                        System.out.println("Time: " + on.getTime());
-                        break;
-                    case 6: // PWheel event (Pitch wheel)
-                        PWheel wheel = (PWheel) ev;
-                        System.out.println("PWheel event:");
-                        System.out.println("Value: " + wheel.getValue());
-                        System.out.println("Time: " + wheel.getTime());
-                        break;
-                    case 7: // PChange event (Program change)
-                        PChange pchange = (PChange) ev;
-                        System.out.println("PChange event:");
-                        System.out.println("Value: " + pchange.getValue());
-                        System.out.println("Time: " + pchange.getTime());
-                        break;
-                    case 8: // SysEx event
-                        SysEx sysex = (SysEx) ev;
-                        System.out.println("SysEx event(s):");
-                        // Note sure what to do here. Has a method to return a list of System Exclusive events
-                        break;
-                    case 16: // Tempo event
-                        TempoEvent tempevt = (TempoEvent) ev;
-                        System.out.println("TempoEvent event:");
-                        System.out.println("Tempo: " + tempevt.getTempo());
-                        System.out.println("Time: " + tempevt.getTime());
-                        break;
-                    case 17: // TimeSig event
-                        TimeSig timesig = (TimeSig) ev;
-                        System.out.println("TimeSig event:");
-                        System.out.println("Time Signature: " + timesig.getNumerator()
-                                + "/" + timesig.getDenominator());
-                        System.out.println("Metronome Pulse: " + timesig.getMetronomePulse());
-                        System.out.println("Thirty-Second Notes per Beat: " + timesig.getThirtySecondNotesPerBeat());
-                        System.out.println("Time: " + timesig.getTime());
-                        break;
-                    case 18: // KeySig event
-                        KeySig key = (KeySig) ev;
-                        System.out.println("KeySig event:" );
-                        System.out.println("Key Signature: " + key.getKeySig());
-                        System.out.println("Key Quality: " + key.getKeyQuality());
-                        System.out.println("Time: " + key.getTime());
-                        break;
-                    case 23: // EndTrack event
-                        EndTrack end = (EndTrack) ev;
-                        System.out.println("EndTrack event:");
-                        System.out.println("Time:" + end.getTime());
-                        break;
-                    default:
-                        System.out.println("Unknown event type.");
-                        break;
-                }
-                ++j;
-            }
         }
-
-
-
+        
         // Checking if file was successfully read.
         if (from_midi.size() != 0) {
             // Might need reworked
@@ -209,14 +93,124 @@ public class MidiReader {
         return notes;
     }
 
-    /*
-    public int parseEvent(Event event) {
 
-        int pitch;
+    public int parseEvents(Vector<Event> vec) {
 
-        CharSequence eventString = event.toString();
+        for (Event ev : vec) { // Loop through Events
 
-        return pitch;
+            short eventID = ev.getID(); // ID determines type of event
+
+            // Needed for chord tracking
+            CPhrase chord = new CPhrase();
+            chord.
+            Vector<Integer> pitches = new Vector<>();
+
+            // We're mostly interested in the NoteOn/NoteOff events for chords.
+            switch (eventID) {
+                /*
+                case 1: // ATouch event (Aftertouch. Polyphonic key pressure)
+                    ATouch touch = (ATouch) ev;
+                    System.out.println("ATouch event:");
+                    break;
+                case 2: // CPres event (Channel pressure)
+                    CPres cpres = (CPres) ev;
+                    System.out.println("CPres event:");
+                    break;
+                case 3: // CChange event (Controller change)
+                    CChange cchange = (CChange) ev;
+                    System.out.println("CChange event:");
+                    break;
+                    */
+                case 4: // NoteOff event
+
+                    NoteOff off = (NoteOff) ev;
+                    System.out.println("NoteOff event:");
+
+                    // Check if more than one NoteOn event
+                    if (pitches.size() > 1) {
+                        // If so, we just read a chord, so att it to CPhrase
+                        chord.addChord(vectorToIntArr(pitches), 1);
+                    }
+
+                    // Clear pitch vector for next chord.
+                    pitches.clear();
+
+                    break;
+                case 5: // NoteOn event
+
+                    NoteOn on = (NoteOn) ev;
+
+                    // Some MIDI files encode NoteOns as NoteOffs with velocity=0
+                    if (on.getVelocity() != 0) {
+
+                        System.out.println("NoteOn event:");
+
+                        // Add pitch value to vector
+                        pitches.add(Short.toUnsignedInt(on.getPitch()));
+
+                    } else {
+                        System.out.println("NoteOff event:");
+
+                        // Check if more than NoteOn event
+                        if (pitches.size() > 1) {
+                            // If so, we just read a chord, so att it to CPhrase
+                            chord.addChord(vectorToIntArr(pitches), 1);
+                        }
+
+                        // Clear pitch vector for next chord.
+                        pitches.clear();
+                    }
+                    break;
+                    /*
+                case 6: // PWheel event (Pitch wheel)
+                    PWheel wheel = (PWheel) ev;
+                    System.out.println("PWheel event:");
+                    break;
+                case 7: // PChange event (Program change)
+                    PChange pchange = (PChange) ev;
+                    System.out.println("PChange event:");
+                    break;
+                case 8: // SysEx event
+                    SysEx sysex = (SysEx) ev;
+                    System.out.println("SysEx event(s):");
+                    // Note sure what to do here. Has a method to return a list of System Exclusive events
+                    break;
+                case 16: // Tempo event
+                    TempoEvent tempevt = (TempoEvent) ev;
+                    System.out.println("TempoEvent event:");
+                    break;
+                case 17: // TimeSig event
+                    TimeSig timesig = (TimeSig) ev;
+                    System.out.println("TimeSig event:");
+                    break;
+                case 18: // KeySig event
+                    KeySig key = (KeySig) ev;
+                    System.out.println("KeySig event:" );
+                    break;
+                case 23: // EndTrack event
+                    EndTrack end = (EndTrack) ev;
+                    System.out.println("EndTrack event:");
+                    break;
+                default:
+                    System.out.println("Unknown event type.");
+                    break;
+                    */
+            }
+        }
+        return 0;
     }
-    */
+
+    // CPhrase.addChord only takes int[] or Note[]
+    // So this method takes a Vector<Integer> of pitchs and converts to an
+    // int[]
+    public int[] vectorToIntArr(Vector<Integer> pitches) {
+
+        int[] ints = new int[pitches.size()];
+
+        for (int k = 0; k < pitches.size(); ++k) {
+            ints[k] = pitches.get(k);
+        }
+
+        return ints;
+    }
 }
