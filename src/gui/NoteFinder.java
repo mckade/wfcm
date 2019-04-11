0a5ef8417768bb1152e27c0708de536cda0d95ee
Robert Randolph <rrandol3@uwyo.edu>
2019-04-10 19:58:52 -0600

    Modifed noteFinder, and fixed bug

--- src/gui/NoteFinder.java
+++ src/gui/NoteFinder.java
@@ -4,34 +4,50 @@
  * @members McKade Umbenhower, Robert Randolph, Taylor Bleizeffer 
  * 
  * Helps find the correct note from a list of notes.
- * First it will take in an array of RNotes.
- * Next it will split up the notes into their respective rowsjava
+ * After given a list of notes to look through it will
+ * - Organize the notes by rows.
  */
 
 package gui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import model.RNote;
 
 public class NoteFinder {
     
-    RNote[] notes;
+    private RNote[] notes;
+    private HashMap<Integer, ArrayList<RNote>> rows;
     
-    public NoteFinder() {
-        
-    }
+    public NoteFinder() {}  // Does nothing
     
-    // Receives a RNote array and organizes the notes
+    // Receives a RNote array and organizes the notes by row
     public void setNotes(RNote[] notes) {
         this.notes = notes;
+        rows = new HashMap<Integer, ArrayList<RNote>>();
+        if (this.notes == null) return;
+        
+        for (RNote note: notes) {
+            if(rows.containsKey(note.y)) {
+                rows.get(note.y).add(note);
+            }
+            else {
+                ArrayList<RNote> rowNotes = new ArrayList<RNote>();
+                rowNotes.add(note);
+                rows.put(note.y, rowNotes);
+            }
+        }
     }
     
     // Returns the note at the given spot.
     // Returns null if no note was found.
     public RNote getNote(int x, int y) {
         if (notes == null) return null;
-        for (RNote note: notes) {
-            if (note.y != y) continue;
-            if (x >= note.x && x <= note.width)
+        if (!rows.containsKey(y)) return null;
+        ArrayList<RNote> rowNotes = rows.get(y);
+        for (RNote note: rowNotes) {
+            if (x >= note.x && x <= note.x + note.width)
                 return note;
         }
         return null;
